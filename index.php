07c554cc1e31e993fda19a6dc874fffe9a986c65
imtedyeh@gmail.com <TedYeh>
2012-08-11 20:15:31 +0800

    phpmysql2

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php # Script 3.4 - index.php
+$page_title = 'Welcome to this Site!';	//set $page_title before include other files.
+include('./includes/header.html');
+?>
+<h1 id="mainhead">Big Header</h1>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>		
+<h2>Subheader</h1>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>
+<p>This is where you'll put the main page conten. This content will differ for each page.</p>
+<?php
+include('./includes/footer.html');
+?>
